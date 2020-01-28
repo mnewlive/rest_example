1617a487cf226f109eae3c9e90b86c2ed1a3a037
mnewlive <morozovvadim91@gmail.com>
2020-01-28 12:28:13 +0200

    Added ClientRepository with ClientController

---
+++ src/main/java/com/mandarine/rest_example/controller/ClientController.java
@@ -0,0 +1,83 @@
+package com.mandarine.rest_example.controller;
+
+import com.mandarine.rest_example.model.Client;
+import com.mandarine.rest_example.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Class in which we implement the logic for processing client requests for endpoints (URIs).
+ */
+@RestController
+public class ClientController {
+
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    /**
+     * This method handles POST requests to the address: "/clients"
+     *
+     * @param client - the value of this parameter is substituted from the request body
+     * @return ResponseEntity - special class for returning responses
+     */
+    @PostMapping(value = "/clients")
+    public ResponseEntity<?> create(@RequestBody Client client) {
+        clientService.create(client);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    /**
+     * Get a list of all clients
+     *
+     * @return ResponseEntity<List<Client>> - returns the status with response body(list of clients)
+     */
+    @GetMapping(value = "/clients")
+    public ResponseEntity<List<Client>> read() {
+        final List<Client> clients = clientService.readAll();
+
+        return clients != null && !clients.isEmpty()
+                ? new ResponseEntity<>(clients, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Getting the client by id
+     *
+     * @param id - any numeric value
+     * @return ResponseEntity - returns the status with response body
+     */
+    @GetMapping(value = "/clients/{id}")
+    public ResponseEntity<Client> read(@PathVariable(name = "id") int id) {
+        final Client client = clientService.read(id);
+
+        return client != null
+                ? new ResponseEntity<>(client, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping(value = "/clients/{id}")
+    public ResponseEntity<?> update(@PathVariable(name = "id") int id, @RequestBody Client client) {
+        final boolean updated = clientService.update(client, id);
+
+        return updated
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
+
+    @DeleteMapping(value = "/clients/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id") int id) {
+        final boolean deleted = clientService.delete(id);
+
+        return deleted
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
+}
